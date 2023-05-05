c22cf7ce11615e5d2f9b4b7457d672f5b725c860
Chinmay <cschandragiri@gmail.com>
2023-05-04 22:38:23 -0700

    Innodb cluster with mysql router

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ Playground for mysql replication and high availability
 
 #### Projects
 - [Leader Replication setup (no high availability)](leader-follower-no-ha/README.md)
-- [Group Replication setup](group-replication/README.md)
+- [Group Replication setup (multi-master)](group-replication-multi-master/README.md)
---
+++ group-replication-with-mysql-router/reader.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+for N in {1..3}
+do
+  until docker exec node$N sh -c 'export MYSQL_PWD=root; mysql -u root -e ";"'
+  do
+      echo "Waiting for node$N database connection..."
+      sleep 4
+  done
+done
+
+while true
+do
+  echo "****************"
+  SQL_QUERY="use playgroundDB; SELECT node, count(1) from code group by node;"
+  docker exec node3 sh -c "export MYSQL_PWD=password; mysql -u user playgroundDB -e '$SQL_QUERY'"
+  sleep 1
+done
