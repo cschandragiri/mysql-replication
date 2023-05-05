1fbafc6a7b96f5d08bb4d59152d8a6eb2d7785bb
Chinmay <cschandragiri@gmail.com>
2023-05-04 23:41:49 -0700

    mysql router setup

--- group-replication-with-mysql-router/writer.sh
+++ group-replication-with-mysql-router/writer.sh
@@ -1,22 +1,13 @@
 #!/bin/bash
 
-for N in {1..3}
-do
-  until docker exec node1 sh -c 'export MYSQL_PWD=root; mysql -u root -e ";"'
-  do
-      echo "Waiting for node$N database connection..."
-      sleep 4
-  done
-done
-
 counter=1000
 while [ $counter -le 20000 ]
 do
-  SQL_QUERY="INSERT INTO code (node, number) VALUES ('node1', $counter)"
+  SQL_QUERY="INSERT INTO code (node, number) VALUES ('router', $counter)"
   CMD='export MYSQL_PWD=password; mysql -u user playgroundDB -e "'
   CMD+="$SQL_QUERY"
   CMD+='"'
   # execute the SQL query
-  docker exec node1 sh -c "$CMD"
+  docker exec router sh -c "$CMD"
   counter=$((counter+1))
 done
